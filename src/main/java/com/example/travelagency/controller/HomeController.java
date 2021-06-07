f5027ddb3514c23d04a7ca62b5db0562fa62c88a
marcinsmoliga <50251884+marcinsmoliga@users.noreply.github.com>
2021-06-07 23:19:54 +0200

    model -> Tour created | form.html, tours.html created

--- src/main/java/com/example/travelagency/controller/HomeController.java
+++ src/main/java/com/example/travelagency/controller/HomeController.java
@@ -1,13 +1,40 @@
 package com.example.travelagency.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.travelagency.model.Tour;
 
 @Controller
 public class HomeController {
+	private List<Tour> tours = new ArrayList<>();
 
 	@GetMapping("/")
 	public String getHome() {
 		return "home";
+	}
+
+	@GetMapping("/addTour")
+	public String showForm(Model model) {
+		model.addAttribute("tour", new Tour());
+		return "form";
+	}
+
+	@PostMapping("/processForm")
+	public String showTourData(@ModelAttribute Tour tour) {
+		tours.add(tour);
+		return "redirect:showOffer";
+	}
+
+	@GetMapping("/showOffer")
+	public String getTours(Model model) {
+		model.addAttribute("tours", tours);
+		return "tours";
 	}
 }
---
+++ src/main/java/com/example/travelagency/model/Tour.java
@@ -0,0 +1,67 @@
+package com.example.travelagency.model;
+
+import java.util.Date;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+public class Tour {
+	public enum Continent {
+		AFRICA, ASIA, EUROPE, NORTH_AMERICA, SOUTH_AMERICA;
+	}
+
+	private String name;
+	private String code;
+	private Continent continent;
+	@DateTimeFormat(pattern = "yyy-MM-dd")
+	private Date date;
+	private int duration;
+	private boolean allInclusive = false;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public Continent getContinent() {
+		return continent;
+	}
+
+	public void setContinent(Continent continent) {
+		this.continent = continent;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public int getDuration() {
+		return duration;
+	}
+
+	public void setDuration(int duration) {
+		this.duration = duration;
+	}
+
+	public boolean isAllInclusive() {
+		return allInclusive;
+	}
+
+	public void setAllInclusive(boolean allInclusive) {
+		this.allInclusive = allInclusive;
+	}
+}
