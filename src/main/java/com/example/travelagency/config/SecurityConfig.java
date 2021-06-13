118bc42754502524d6de9cc26cda0316eb45a6e9
marcinsmoliga <50251884+marcinsmoliga@users.noreply.github.com>
2021-06-13 17:44:09 +0200

    SeciurityConfig added | custom login page created

---
+++ src/main/java/com/example/travelagency/config/SecurityConfig.java
@@ -0,0 +1,52 @@
+package com.example.travelagency.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Bean
+	public PasswordEncoder passwordEncoder( ){
+		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth
+				.inMemoryAuthentication()
+				.withUser("admin").password(passwordEncoder().encode("admin")).roles("ADMIN", "EMPLOYEE")
+				.and()
+				.withUser("employee").password(passwordEncoder().encode("employee")).roles("EMPLOYEE")
+				.and()
+				.withUser("client").password(passwordEncoder().encode("client")).roles("CLIENT");
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+				.antMatchers("/", "/login")
+					.permitAll()
+				.and()
+					.formLogin()
+					.loginPage("/login")
+					.loginProcessingUrl("/checkUserAccount")
+					.defaultSuccessUrl("/")
+					.permitAll()
+				.and()
+					.logout()
+					.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+					.logoutSuccessUrl("/")
+					.invalidateHttpSession(true)
+					.permitAll();
+
+	}
+}
